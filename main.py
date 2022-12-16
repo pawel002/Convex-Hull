5000eed9eb387aab9b900cd05784615a43dbe96a
pawel002 <pawel.gandalf@gmail.com>
2022-12-16 15:02:55 +0100

    benchmarks

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 from utils.generate import *
 from utils.viz import *
 
-points = genUniformOnSquare(10, 20, 20)
-plotPoints(points)
-
-
---
+++ utils/benchmark.py
@@ -0,0 +1,28 @@
+from time import time
+
+def benchmark(hullAlgorithm, timeOfBenchamrk, generator, *args):
+    '''Funkcja przyjmuje algorytm tworzacy otoczke, czas po jakim chcemy przeprowadzic benchmark w sekundach oraz
+       argumenty generatora. Dzieki temu unikniemy sytuacji zadania za duzego zbioru danych. Wyniki zostają uśrednione.'''
+
+    startTime = time()
+    cumTime = 0
+
+    count = 0
+    while True:
+
+        if time() - startTime() > timeOfBenchamrk:
+            break
+        
+        points = generator(*args)
+
+        benchStart = time()
+        hullAlgorithm(points)
+        benchEnd = time()
+
+        cumTime += benchEnd - benchStart
+        count += 1
+    
+    print("TESTS FINISHED" + 20*"-")
+    print("Executed", count, "tests.")
+    print("Average time per test:", cumTime / count)
+
