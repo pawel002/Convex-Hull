bb1fd64df1584047317c8ca83fa0b8bcd24750cd
pawel002 <pawel.gandalf@gmail.com>
2022-12-26 20:50:10 +0100

    update

---
+++ convexhulls.py
@@ -0,0 +1,11 @@
+from algorithms.chan import chan, chanVis
+from algorithms.grahams import grahams, grahamsVis
+from algorithms.increment import increment, incrementVis
+from algorithms.jarvis import jarvis, jarvisVis
+from algorithms.quickhull import quickhull, quickhullVis
+from algorithms.upperlowerhull import upperlower, upperlowerVis
+
+from utils.generate import *
+from utils.viz import *
+from utils.benchmark import *
+
--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-from algorithms.chan import chan, chanVis
-from algorithms.grahams import grahams, grahamsVis
-from algorithms.increment import increment, incrementVis
-from algorithms.jarvis import jarvis, jarvisVis
-from algorithms.quickhull import quickhull, quickhullVis
-from algorithms.upperlowerhull import upperlower, upperlowerVis
-
-from utils.generate import *
-from utils.viz import *
-from utils.benchmark import *
+from convexhulls import *
 
 import copy
 import time
 
 
-# points = genUniformRectangle(-10, 10, -20, 30, 20)
-# points = genUniformCirle(10, 10, 10, 100)
-# points = genUniformOnRectangle([-10, -10], [20, 30], 100)
-points = genUniformOnSquare(10, 20, 20)
+points1 = genUniformRectangle(-10, 10, -20, 30, 20)
+points2 = genUniformCirle(10, 10, 10, 100)
+points3 = genUniformOnRectangle(-10, -10, 20, 30, 100)
+points4 = genUniformOnSquare(10, 20, 20)
 
 # bigPoints = genUniformOnRectangle([-10, -10], [20, 30], 10000)
 # bigPoints = genUniformOnSquare(10, 500, 500)
 # bigPoints = genUniformCirle(10, 10, 10, 1000)
 
-# hull = increment(bigPoints)
-# print(len(hull))
-
+# points = genUniformRectangle(-10, 10, -20, 30, 20)
+# hull = increment(points)
 # plotHull(points, hull)
 
+points = genUniformRectangle(-10, 10, -10, 10, 10)
 visHull(incrementVis, points)
 
 # benchmark(increment, 2, genUniformCirle, 10, 10, 10, 10000)
 # benchmark(increment, 2, genUniformRectangle, -10, 10, -10, 10, 10000)
-# benchmark(upperlower, 2, genUniformRectangle, -10, 10, -10, 10, 10000)
+# benchmark(increment, 2, genUniformRectangle, -10, 10, -10, 10, 10000)
