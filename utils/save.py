a59fcdc33cbcd9253ca7d6c49786ff4f82ea3fbd
pawel002 <pawel.gandalf@gmail.com>
2022-12-27 18:14:09 +0100

    save update

--- utils/save.py
+++ utils/save.py
@@ -4,10 +4,9 @@ def saveList(points, filename):
     with open(filename, 'w') as outfile:
         for point in points:
             outfile.write(str(point[0]) + " " + str(point[1]) + "\n")
-        outfile.close()
+
 
 def readList(filename):
-    arr = []
     with open(filename, 'r') as infile:
         lines = infile.readlines()
         arr = [[0, 0] for x in range(len(lines))]
@@ -15,8 +14,10 @@ def readList(filename):
             line.replace('\n', '')
             x = line.split(' ')
             arr[i][0], arr[i][1] = float(x[0]), float(x[1])
-        infile.close()  
+
         return arr
+    
+    return []
     
     
     
